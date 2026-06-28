e3fd99f594c9413d23efd7ad2e9bc1e407ec77e6
efficientcto <efficientcto@gmail.com>
2026-06-28 12:47:26 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://efficientcto.com',
+  site: 'https://efficientcto.github.io/efficientcto/',
   output: 'static'
 });
